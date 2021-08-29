44812347e36291c67d550b832c8d60b8c2871593
skmonsterr <ctjoaomartins@gmail.com>
2021-08-29 17:44:53 -0300

    Create a compontent - Sidebar

---
+++ src/components/ServerList/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ServerButton from "../ServerButton";
+import { Container, Separator } from "./styles";
+
+const ServerList: React.FC = () => {
+  return (
+    <Container>
+      <ServerButton isHome />
+
+      <Separator />
+
+      <ServerButton />
+      <ServerButton hasNotifications />
+      <ServerButton mentions={3} />
+      <ServerButton hasNotifications />
+      <ServerButton />
+      <ServerButton mentions={80} />
+      <ServerButton />
+    </Container>
+  );
+};
+
+export default ServerList;
