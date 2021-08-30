b0b1b3bd35f91172167d5fda188106f28351823f
skmonsterr <ctjoaomartins@gmail.com>
2021-08-29 22:56:26 -0300

    Updates

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,31 +1,23 @@
-import React, { useRef, useEffect } from "react";
+import React from "react";
 
 import ChannelMessage, { Mention } from "../ChannelMessage";
 
 import { Container, Messages, InputWrapper, Input, InputIcon } from "./styles";
 
 const ChannelData: React.FC = () => {
-  const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
-
-  useEffect(() => {
-    const div = messagesRef.current;
-
-    if (div !== null && div !== undefined) {
-      div.scrollTop = div.scrollHeight;
-    }
-  }, [messagesRef]);
-
   return (
     <Container>
       <Messages>
-        {Array.from(Array(15).keys()).map((n) => (
-          <ChannelMessage
-            key={n}
-            author="João Martins"
-            date="03/06/2021"
-            content="Hoje é meu aniversário!"
-          />
-        ))}
+        <ChannelMessage
+          author="João Martins"
+          date="03/06/2021"
+          content="Hoje é meu aniversário!"
+        />
+        <ChannelMessage
+          author="João Martins"
+          date="04/06/2021"
+          content="Testando o discord"
+        />
         <ChannelMessage
           author="My API Bot"
           date="03/06/2021"
